c17195cdd1b8dccee99af47551621f44cf946d46
dufrane <dmitriy.vasilenko@gmail.com>
2022-08-16 14:27:40 +0400

    made some corrections of UI

--- Dice/Dicee/DiceViewController.swift
+++ Dice/Dicee/DiceViewController.swift
@@ -1,28 +1,31 @@
 //
 //  ViewController.swift
-//  Dicee-iOS13
+//  Dicee
 //
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Dmytro Vasylenko on 05.08.2022.
+//
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class DiceViewController: UIViewController {
     
+    @IBOutlet weak var rollButton: UIButton!
     @IBOutlet weak var diceImageViewOne: UIImageView!
-    
     @IBOutlet weak var diceImageViewTwo: UIImageView!
     
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rollButton.layer.cornerRadius = 10
+//        rollButton.backgroundColor = UIColor.systemRed
+    }
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
         
         diceImageViewOne.image = diceArray.randomElement()
         diceImageViewTwo.image = diceArray.randomElement()
-        
     }
-    
 }
 
